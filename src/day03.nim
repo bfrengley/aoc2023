0e248af11def5bd002dcdb3e2205234202d870a3
Ben Frengley <ben.frengley@gmail.com>
2023-12-04 11:59:07 +1300

    Day 03 part 2

--- src/day03.nim
+++ src/day03.nim
@@ -17,11 +17,14 @@ const
   input = staticRead("data/day03.txt")
 
 type
+  Pos = tuple[x: int, y: int]
   N = object
     n: int
     y: int
     xs: Slice[int]
-  Pos = tuple[x: int, y: int]
+  Sym = object
+    sym: char
+    pos: Pos
 
 iterator neighbours(pos: Pos): Pos =
   for y in (pos.y - 1)..(pos.y + 1):
@@ -30,12 +33,13 @@ iterator neighbours(pos: Pos): Pos =
         if x >= 0:
           yield (x: x, y: y)
 
-func part1(input: string): int =
+proc day3(input: string) =
   let width = input.find('\n')
   var
     grid = newSeqWith(input.countLines(), newSeq[Option[N]](width))
-    symbols = newSeq[Pos](0.Natural)
-
+    symbols = newSeq[Sym]()
+    part1 = 0
+    part2 = 0
 
   for y, line in enumerate(input.splitLines(false)):
     var x = 0
@@ -44,7 +48,7 @@ func part1(input: string): int =
         # there might be some symbols in here
         for (symTok, isDots) in tok.tokenize({'.'}):
           if not isDots:
-            add(symbols, (x: x, y: y))
+            add(symbols, Sym(sym: symTok[0], pos: (x: x, y: y)))
           x += symTok.len
       else: # number
         let n = N(n: parseInt(tok), y: y, xs: x..<(x + tok.len))
@@ -53,16 +57,24 @@ func part1(input: string): int =
         x += tok.len
 
   for symbol in symbols:
-    let ns = collect(newSeqOfCap(9.Natural)):
-      for (x, y) in symbol.neighbours:
-        let maybeN = grid[y][x]
-        if maybeN.isSome:
-          maybeN.get()
+    let ns = block:
+      let ns = collect(newSeqOfCap(9.Natural)):
+        for (x, y) in symbol.pos.neighbours:
+          let maybeN = grid[y][x]
+          if maybeN.isSome:
+            maybeN.get()
+      ns.deduplicate(true)
 
-    for n in ns.deduplicate(true):
-      result += n.n
+    for n in ns:
+      part1 += n.n
+
+    if symbol.sym == '*' and ns.len == 2:
+      part2 += ns[0].n * ns[1].n
+
+  echo "Part 1: ", part1
+  echo "Part 2: ", part2
 
 when isMainModule:
   var data = input
   stripLineEnd(data)
-  echo "Part 1: ", part1(data)
+  day3(data)
